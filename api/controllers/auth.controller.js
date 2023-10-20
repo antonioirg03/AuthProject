e39e1cf39751affffff9cb46f78c4137eaa23b33
antonio <>
2023-10-20 13:45:58 +0200

    create signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -13,5 +14,32 @@ export const signup = async (req, res, next) => {
     });
   } catch (err) {
     next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found!"));
+    }
+
+    const isValidPassword = bcrypt.compareSync(password, validUser.password);
+    if (!isValidPassword) {
+      return next(errorHandler(400, "Invalid credentials!"));
+    }
+    const token = jwt.sign({ userId: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 1000 * 60 * 60 * 24 * 7);
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: expiryDate,
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
